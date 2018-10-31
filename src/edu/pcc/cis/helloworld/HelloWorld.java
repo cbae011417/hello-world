77974315ffc7e1fe538c2c93a3ef726fe6306c1c
Courtney Bae <cs.bae92@gmail.com>
2018-10-30 20:55:13 -0700

    Added date to be printed out

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,5 +1,7 @@
 package edu.pcc.cis.helloworld;
 
+import java.util.Date;
+
 /**
  * Created by Courtney Bae on 2018.10.30
  */
@@ -8,6 +10,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println(new Date());
     }
 
 }
